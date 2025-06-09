cdd0f0b451d2ac500dcfaaf55caf53c4e359a2d1
raedhashmi <raedhashmi15@gmail.com>
2025-06-10 00:08:27 +0500

    Initial Commit

---
+++ ZWear.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_file('templates/index.html')
+
+@app.route('/resources/<path:path>')
+def resources(path):
+    return send_file(f'resources/{path}')
+
+if __name__ == '__main__':
+    app.run()
